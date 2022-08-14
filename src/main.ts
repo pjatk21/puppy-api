e24fb0de46c5209236bc6d78be0cc7c7fd19ffb1
Krystian Postek <krystian@postek.eu>
2022-08-14 12:27:15 +0200

    Disable x-powered-by

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core'
+import {
+  ExpressAdapter,
+  NestExpressApplication,
+} from '@nestjs/platform-express'
 import { AppModule } from './app.module'
 
-async function bootstrap() {
-  const app = await NestFactory.create(
-    AppModule,
-    { cors: true },
-    // new FastifyAdapter(),
-  )
-  await app.listen(3000)
-}
-await bootstrap()
+const adapter = new ExpressAdapter()
+adapter.disable('x-powered-by')
+
+const app = await NestFactory.create<NestExpressApplication>(
+  AppModule,
+  adapter,
+  { cors: true },
+  // new FastifyAdapter(),
+)
+await app.listen(3000)
