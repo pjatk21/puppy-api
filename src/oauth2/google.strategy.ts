5444e5f239986c2e8ca431ea85e4b54fc1e610c0
Krystian Postek <krystian@postek.eu>
2022-08-23 23:12:11 +0200

    Improvements to google login strategy

--- src/oauth2/google.strategy.ts
+++ src/oauth2/google.strategy.ts
@@ -1,34 +1,53 @@
 import { PassportStrategy } from '@nestjs/passport'
-import { Strategy, VerifyCallback } from 'passport-google-oauth20'
+import { Strategy, Profile, StrategyOptions } from 'passport-google-oauth20'
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
+import { UsersService } from 'src/users/users.service'
+import { UserSession } from '@prisma/client'
+import Joi from 'joi'
+
+export type SafeProfile = {
+  emails: Array<{ value: string }>
+  displayName: string
+}
+
+const profileValidationSchema = Joi.object<SafeProfile>({
+  emails: Joi.array().min(1).required(),
+  displayName: Joi.string().required(),
+} as Record<keyof Profile, Joi.Schema>)
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   public constructor(
-    private readonly config: ConfigService<NodeJS.ProcessEnv, true>,
+    config: ConfigService<NodeJS.ProcessEnv, true>,
+    private readonly users: UsersService,
   ) {
     super({
       clientID: config.get('GOOGLE_OAUTH_CLIENT_ID'),
       clientSecret: config.get('GOOGLE_OAUTH_CLIENT_SECRET'),
       callbackURL: config.get('GOOGLE_OAUTH_CALLBACK_URL'),
       scope: ['email', 'profile'],
-    })
+      skipUserProfile: false,
+    } as StrategyOptions)
   }
 
   public async validate(
     accessToken: string,
     refreshToken: string,
-    profile: any,
-  ): Promise<any> {
-    const { name, emails, photos } = profile
-    const user = {
-      email: emails[0].value,
-      firstName: name.givenName,
-      lastName: name.familyName,
-      picture: photos[0].value,
-      accessToken,
-    }
-    return user
+    profile: Profile,
+  ) {
+    const safeProfile: SafeProfile =
+      await profileValidationSchema.validateAsync(profile, {
+        stripUnknown: true,
+      })
+
+    // const user = await this.users.getOrCreateUser({
+    //   email: safeProfile.emails[0].value,
+    //   name: safeProfile.displayName,
+    // })
+
+    //console.log(safeProfile, user)
+
+    return safeProfile
   }
 }
