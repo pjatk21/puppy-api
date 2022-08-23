c8ddc22f924f85deca90b2320a7dee9f8c5a64dd
Krystian Postek <krystian@postek.eu>
2022-08-24 01:27:25 +0200

    Add query for user's scrapers

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -2,13 +2,17 @@ import { Res, UseGuards } from '@nestjs/common'
 import { Args, Query, Resolver, ResolveField, Mutation } from '@nestjs/graphql'
 import { User } from '@prisma/client'
 import { BearerGuard } from 'src/oauth2/guards/gql/bearer.guard'
+import { PrismaService } from 'src/prisma/prisma.service'
 import { CurrentUser } from './decorators/gql.decorator'
 import { UsersService } from './users.service'
 
 @Resolver('User')
 @UseGuards(BearerGuard)
 export class UsersResolver {
-  public constructor(private readonly users: UsersService) {}
+  public constructor(
+    private readonly users: UsersService,
+    private readonly prisma: PrismaService,
+  ) {}
 
   @Query('me')
   public async user(@CurrentUser() user: User) {
@@ -20,6 +24,15 @@ export class UsersResolver {
     const { groups } = await this.users.getGroups(user)
     const groupsArray = groups.map(({ group }) => group)
     return groupsArray
+  }
+
+  @ResolveField('scrapers')
+  public async scrapers(@CurrentUser() user: User) {
+    return this.prisma.scrapper.findMany({
+      where: {
+        ownerId: user.id,
+      },
+    })
   }
 
   @Mutation('setGroups')
